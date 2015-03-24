dd8c8fc56fc05491c146a66d6cdb615aa65e7a85
Michael Bateman <michael.bateman.com@gmail.com>
2015-03-24 15:08:31 +0000

    removed unused imports in test

--- src/test/java/com/metability/addressbook/AddressBookTest.java
+++ src/test/java/com/metability/addressbook/AddressBookTest.java
@@ -4,10 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.Comparator;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
